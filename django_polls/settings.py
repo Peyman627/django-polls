8f82b409063017b554217f16aa944bfdf4e3905f
Peyman Hassani <hassani.peyman627@gmail.com>
2021-08-21 16:15:49 +0430

    mysql db and python-decouple configured

--- django_polls/settings.py
+++ django_polls/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zv&*)13*!)k$lfglvgal-tk+-_56uurho2=oaksr9_m*0-@qur'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -73,8 +74,12 @@ WSGI_APPLICATION = 'django_polls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polls',
+        'USER': 'root',
+        'PASSWORD': 'jsmac627',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
